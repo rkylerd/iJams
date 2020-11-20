4bca3f109df0ca1ff46fb66fa79b453ec50a99b7
Kyler <rkylerd06@Outlook.com>
2020-11-20 16:32:21 -0700

    Added music video tile component.

---
+++ src/components/MVideoTile.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <video width="200px" height="auto" controls>
+          <source :src="mvid.previewUrl" type=""/>
+      </video>
+      <div class="mvideo-info">
+          <span>{{mvid.trackCensoredName}}</span><br>
+          <span>{{mvid.artistName}}</span>
+      </div>
+  </div>  
+</template>
+
+<script>
+
+import { addToPlaylist, cutLength, updateMusicIcon } from '@/shared/logic'
+import { goToAlbum, filterArtist } from '@/shared/navigation'
+import App from '@/App.vue'
+import router from '@/router'
+export default {
+  name: "MVideoTile",
+  props: {
+    mvid: Object
+  }, 
+  methods: {
+      addToPlaylist: addToPlaylist,
+      goToAlbum: goToAlbum,
+      cutLength: cutLength,
+      filterArtist: filterArtist
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
